484b2eefd6f0d91baa9352f409c608e94bcee8ef
Ezekiel Kibiego <kibiezekiel@gmail.com>
2022-07-29 08:16:48 +0300

    changing properties file

--- src/main/java/com/kibiegoInBring/limitsservice/controller/LimitController.java
+++ src/main/java/com/kibiegoInBring/limitsservice/controller/LimitController.java
@@ -1,15 +1,21 @@
 package com.kibiegoInBring.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kibiegoInBring.limitsservice.bean.Limits;
+import com.kibiegoInBring.limitsservice.configuration.Configuration;
 
 @RestController
 public class LimitController {
 
+	@Autowired
+	private Configuration configuration;
 	@GetMapping("/limits")
 	public Limits retrieveLimits() {
-		return new Limits(1,1000);
+		return new Limits(configuration.getMinimum(),
+				configuration.getMaximum());
+//		return new Limits(1,1000);
 	}
 }
